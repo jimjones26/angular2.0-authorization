d25250000c7686c6f4995bf8535c617481e8cd9c
jimjones26 <jimjones26@gmail.com>
2016-09-30 14:43:21 -0400

    generate structure for protected component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/protected/protected.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ProtectedComponent } from './protected.component';
+
+describe('Component: Protected', () => {
+  it('should create an instance', () => {
+    let component = new ProtectedComponent();
+    expect(component).toBeTruthy();
+  });
+});
